0c6a0274b68f211deace7374f8058aaa5d5227a3
Molly McCarron <mccarronmollye@gmail.com>
2018-11-06 20:42:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
 if array.length == 2
-  array.join(" and ")
-else
-  array.insert(-2, "and")
+  return "#{array[0]} and #{array[1]}"
+elsif 2 < array.length
+  array[-1].insert(0, "and ")
 end
 array.join(", ")
-end
+end 
